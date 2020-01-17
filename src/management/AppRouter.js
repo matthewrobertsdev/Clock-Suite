ce2c259066d1c2f3d9a582c1a438122f640f276a
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-01-17 17:06:31 -0500

    Added AboutPage

--- src/management/AppRouter.js
+++ src/management/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../view/Header';
 import Home from '../view/Home';
+import AboutPage from '../view/AboutPage';
 //homepage
 //support
 //about
@@ -13,7 +14,7 @@ export default function AppRouter() {
                 <Header />
                 <Switch>
                 <Route path="/" exact component={Home} />
-                    <Route path="/about" exact component={Home} />
+                    <Route path="/about" exact component={AboutPage} />
                     <Route path="/contact" exact component={Home} />
                     <Route path="/privacy" exact component={Home} />
                     <Route component={Home} />
