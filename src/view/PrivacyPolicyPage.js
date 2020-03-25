9448e0fdbe7e32c365da92b4e6e503730f9888b1
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-03-25 10:40:35 -0400

    Changed dark mode yellow, privacy policy, and main-text style

--- src/view/PrivacyPolicyPage.js
+++ src/view/PrivacyPolicyPage.js
@@ -5,9 +5,9 @@ class UnconnectedPrivacyPolicyPage extends React.Component {
     render(){
         return (<div className={'main-background main-'+this.props.colorClass}>
             <br></br><h1 className='main-h-1'>Privacy Policy</h1>
-            <div className='main-text'>Celeritas Apps collects log data as allowed by the user of Adjustable Clock 
-            and provided by Apple.  We only use data as provided by Apple and do not collect any data on our own 
-            about the users of Adjustable Clock.</div><br></br><br></br><br></br></div>);
+            <div className='main-text'>I, Matt Roberts, collect log data as allowed by the user of Clock Suite 
+            and provided by Apple.  I do not collect any data on my own 
+            from the use of Clock Suite by its users.</div><br></br><br></br><br></br></div>);
     }
 }
 const PrivacyPolicyPage=connect(mapStateToProps, null)(UnconnectedPrivacyPolicyPage)
