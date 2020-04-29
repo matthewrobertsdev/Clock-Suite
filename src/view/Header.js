59aa9c713ccbd011f64554f4f8dd475e45f50f32
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-04-29 14:05:56 -0400

    Header now is a functional component and changed dark mode yellow

--- src/view/Header.js
+++ src/view/Header.js
@@ -1,33 +1,27 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import {useSelector } from 'react-redux'
 import NavMenu from './NavMenu'
 import MenuBars from './MenuBars'
-import {changeColor, displayMenu} from '../management/DefaultReducerActions'
+import {displayMenu} from '../management/DefaultReducerActions'
 import './App.css'
 import './NavigationStyle.css'
 import './Colors.css'
-const mapStateToProps = (state) => { return { colorClass: state.misc.colorClass, 
-  menuDisplayed: state.misc.menuDisplayed } };
-const mapDispatchToProps = dispatch => {
-  return { changeColor: () => { dispatch(changeColor()); },
-  displayMenu: (displayed) => { dispatch(displayMenu(displayed)); } }};
-class UnconnectedHeader extends React.Component{
-  componentDidMount(){
-    this.props.changeColor();
-    window.addEventListener('resize', ()=>this.closeMenuAsNeeded());
-    window.document.title="Clock Suite"
-  }
-    render(){
-      document.body.classList=this.props.colorClass
-      return(<nav className={"navigation-bar "+this.props.colorClass}><NavMenu/><span className={"navigation-bar navigation-list hide-for-not-small "+this.props.colorClass}>
+
+const Header = ()=> {
+  const colorClass=useSelector(state => state.misc.colorClass)
+  const menuDisplayed=useSelector(state => state.misc.menuDisplayed)
+  useEffect(()=> {
+  window.addEventListener('resize', () => closeMenuAsNeeded());
+  window.document.title="Clock Suite"; document.body.classList=colorClass})
+    return (
+      //
+      <nav className={"navigation-bar "+colorClass}><NavMenu/><span className={"navigation-bar navigation-list hide-for-not-small "+colorClass}>
         <a className={"top-navigation-item float-left top-navigation-link"} 
-    href=''>Clock Suite</a><MenuBars/><br></br><NavMenu sideMenu={true}/></span></nav>);
-    }
-    closeMenuAsNeeded(){
-      if(this.props.menuDisplayed&&window.innerWidth>600){
-        this.props.displayMenu(false)
+    href=''>Clock Suite</a><MenuBars/><br></br><NavMenu sideMenu={true}/></span></nav>)
+    function closeMenuAsNeeded(){
+      if(menuDisplayed&&window.innerWidth>600){
+        displayMenu(false)
       }
     }
 }
-const Header=connect(mapStateToProps, mapDispatchToProps)(UnconnectedHeader)
 export default Header;
