bd2b1017344b745d01fd1c78fcdf675d7ded49ca
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2021-09-07 16:44:58 -0400

    reworded privacy policy more

--- src/pages/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage.js
@@ -8,7 +8,8 @@ const PrivacyPolicyPage = () => {
                 Privacy Policy
                 </h1>
             <h2 className='main-text'>
-                Clock Suite does not collect data from its users, and doesn't connect to the internet.
+                Clock Suite does not collect data from its users, and doesn't connect to the internet.  
+                Any changes to its privacy policy will be reflected here.
             </h2>
             <br></br><br></br><br></br>
             </main>);
